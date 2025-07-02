cfcc3327d2799ad9f82dc3b6130a76e7d27b1baa
Blele2024 <159965055+Blele2024@users.noreply.github.com>
2025-07-02 09:46:10 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1 style={{ color: "#e67fae" }}>Veneza Cosméticos</h1>
-      <p>Loja em construção com carrinho, painel admin e pagamento.</p>
-    </div>
-  );
-}
